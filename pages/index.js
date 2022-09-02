cbba3f2bd2ff05448f0d6e1c1215feb056509335
jsan <ahsan.wtc@gmail.com>
2022-09-02 16:14:54 +0200

    getting data in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import MeetupList from '../components/meetups/MeetupList';
 import { getAllEvents } from '../dummy-data';
 
-export default function Home() {
+export default function Home(props) {
+  const { meetups } = props;
   return (
-    <MeetupList meetups={getAllEvents()} />
+    <MeetupList meetups={meetups} />
   )
 }
+
+export async function getStaticProps() {
+  /* fetch data from an api */
+  /* get data from db */
+  /* this code runs on server and not on client side */
+
+  return {
+    props: {
+      meetups: getAllEvents()
+    }
+  };
+};
