1a99b729554abd25510629f39bbfd3eeb033d6bb
jsan <ahsan.wtc@gmail.com>
2022-09-02 15:23:36 +0200

    adding layout to the app

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import MeetupList from '../components/meetups/MeetupList';
 import { getAllEvents } from '../dummy-data';
 
-
-const MEETUPS = [];
-
 export default function Home() {
   return (
     <MeetupList meetups={getAllEvents()} />
