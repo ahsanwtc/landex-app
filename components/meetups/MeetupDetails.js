aad112ec8ed62d91126990d625ecd0ce644ce563
jsan <ahsan.wtc@gmail.com>
2022-09-02 15:56:55 +0200

    adding style to detail page

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,14 +1,15 @@
 import { Fragment } from 'react';
+import styles from './MeetupDetails.module.css';
 
 const MeetupDetails = props => {
   const { image, title, address, description } = props;
   return (
-    <Fragment>
+    <section className={styles.detail}>
       <img src={image} alt={title}/>
       <h1>{title}</h1>
       <address>{address}</address>
       <p>{description}</p>
-    </Fragment>
+    </section>
   );
 };
 
