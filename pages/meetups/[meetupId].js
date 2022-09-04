56e749e2caaf0042c10f868be9575cd9c39f0fd4
jsan <ahsan.wtc@gmail.com>
2022-09-04 15:06:13 +0200

    pages done

--- pages/meetups/[meetupId].js
+++ pages/meetups/[meetupId].js
@@ -1,35 +1,45 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
 import MeetupDetailsReact from "../../components/meetups/MeetupDetails";;
 
-const MeetupDetails = () => {
-  return <MeetupDetailsReact 
-    image='' title='' address='' description='This is the event description'
-  />;
+const MeetupDetails = ({ meetup }) => {
+  return <MeetupDetailsReact {...meetup} />;
 };
 
 export const getStaticProps = async context => {
   const { params: { meetupId }} = context;
-  console.log({meetupId});
+  
+  const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.hhxotqv.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri);
+  const db = client.db(process.env.DATABASE_NAME);
+  const collection = db.collection('meetups');
+  const meetup = await collection.findOne({ _id: ObjectId(meetupId) });
+  meetup.id = meetup._id.toString();
+  meetup._id = '';
+  await client.close();
 
   return {
     props: {
-      meetup: {
-        image: '/images/coding-event.jpg',
-        id: meetupId,
-        title: 'Some Event',
-        address: 'New Street 10, 666 Strage City',
-        description: 'This is the event description'
-      }
+      meetup
     }
   };
 };
 
 export const getStaticPaths = async () => {
+  const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.hhxotqv.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri);
+  const db = client.db(process.env.DATABASE_NAME);
+  const collection = db.collection('meetups');
+  const paths = (await collection.find({}, { _id: 1 }).toArray()).map(meetup => ({ params: { meetupId: meetup._id.toString() }}));
+  await client.close();
+
   return {
-    paths: [
-      { params: { meetupId: 'e1' }},
-      { params: { meetupId: 'e2' }},
-      { params: { meetupId: 'e3' }}
-    ],
+    // paths: [
+    //   { params: { meetupId: 'e1' }},
+    //   { params: { meetupId: 'e2' }},
+    //   { params: { meetupId: 'e3' }}
+    // ],
+    paths,
     fallback: false
   };
 };
