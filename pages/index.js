a0562b9555b24c2820c4a61cea669d5049d6b4e0
jsan <ahsan.wtc@gmail.com>
2022-09-04 14:53:02 +0200

    pulling data from db

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-import { getAllEvents } from '../dummy-data';
 
 export default function Home(props) {
   const { meetups } = props;
@@ -13,9 +13,16 @@ export async function getStaticProps() {
   /* get data from db */
   /* this code runs on server and not on client side */
 
+  const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.hhxotqv.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri);
+  const db = client.db(process.env.DATABASE_NAME);
+  const collection = db.collection('meetups');
+  const meetups = (await collection.find().toArray()).map(meetup => ({ ...meetup, id: meetup._id.toString(), _id: '' }));
+  await client.close();
+
   return {
     props: {
-      meetups: getAllEvents()
+      meetups
     },
     revalidate: 10
   };
