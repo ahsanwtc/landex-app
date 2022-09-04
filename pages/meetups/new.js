48a045145665a62732ee0a0853e1e5e63e36e5b8
jsan <ahsan.wtc@gmail.com>
2022-09-04 14:41:29 +0200

    fixing api route

--- pages/meetups/new.js
+++ pages/meetups/new.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 export default function NewMeetup() {
-  const addMeetupHandler = meetup => {
+  const router = useRouter();
+  const addMeetupHandler = async meetup => {
     console.log(meetup);
+    const response = await fetch('/api/meetups', {
+      method: 'POST',
+      body: JSON.stringify(meetup),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.replace('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
