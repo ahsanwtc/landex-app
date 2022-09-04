4298a3df640aa4a5615dbd43dcb63cb5f715adfc
jsan <ahsan.wtc@gmail.com>
2022-09-04 14:27:20 +0200

    setting up paths for the pre rendered pages

--- pages/meetups/[meetupId].js
+++ pages/meetups/[meetupId].js
@@ -2,8 +2,36 @@ import MeetupDetailsReact from "../../components/meetups/MeetupDetails";;
 
 const MeetupDetails = () => {
   return <MeetupDetailsReact 
-    image='/images/coding-event.jpg' title='Some Event' address='New Street 10, 666 Strage City' description='This is the event description'
+    image='' title='' address='' description='This is the event description'
   />;
+};
+
+export const getStaticProps = async context => {
+  const { params: { meetupId }} = context;
+  console.log({meetupId});
+
+  return {
+    props: {
+      meetup: {
+        image: '/images/coding-event.jpg',
+        id: meetupId,
+        title: 'Some Event',
+        address: 'New Street 10, 666 Strage City',
+        description: 'This is the event description'
+      }
+    }
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { meetupId: 'e1' }},
+      { params: { meetupId: 'e2' }},
+      { params: { meetupId: 'e3' }}
+    ],
+    fallback: false
+  };
 };
 
 export default MeetupDetails;
