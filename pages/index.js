05a31f3ecb73c8fcd254313f38c7a8eadaa18a43
jsan <ahsan.wtc@gmail.com>
2022-09-13 13:47:14 +0200

    creating Hero component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Hero } from '../components';
 
 export default function Home() {
   return (
-    <Head>
-      <title>Landex App</title>
-      <meta name='' content='' />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div>
+      <Head>
+        <title>Landex App</title>
+        <meta name='' content='' />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Hero heading='Landex App' message='Decentralized marketplace for property.' />
+    </div>
   )
 }
