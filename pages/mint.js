14e125739ca847d24404dcefe499f869549818a5
jsan <ahsan.wtc@gmail.com>
2022-09-14 14:01:44 +0200

    adding mint page

---
+++ pages/mint.js
@@ -0,0 +1,11 @@
+import { Hero } from "../components";
+
+const Mint = () => {
+  return (
+    <div>
+      <Hero heading={'Mint your NFT now'} message='You can mint an NFT' />
+    </div>
+  );
+};
+
+export default Mint;
